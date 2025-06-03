bbd920f1992eae0b5e8552a198b387e381c1e4d1
muchailouzivert <midzkage@gmail.com>
2025-06-03 15:29:31 +0300

    security updaye

--- bikeshop/bikeshop/settings.py
+++ bikeshop/bikeshop/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6_j-%6_8+(u7+tkqmok*hh!hitiauc3@38d6b@u@8=p71wgf2_'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-key-for-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
